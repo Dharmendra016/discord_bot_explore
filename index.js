6ba9a324f8c36f246cc121d159960a13a47d404f
dharmendra016 <dharmendra.078bct016@tcioe.edu.np>
2024-01-09 00:48:16 +0545

    integration image generator ai with discord

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("node:path");
 const { Client, Events, GatewayIntentBits ,SlashCommandBuilder } = require('discord.js');
 const { REST, Routes } = require('discord.js');
 const app = express(); 
+const router = express.Router() ; 
 require("dotenv").config();
 
 const  token = process.env.TOKEN; 
@@ -20,10 +21,16 @@ const cmd = require("./commands/utility/ping");
 
 const commands = []
 commands.push(cmd.data.toJSON());
-const dat = new SlashCommandBuilder()
+let dat = new SlashCommandBuilder()
 .setName('hii')
 .setDescription('greet');
 commands.push(dat.toJSON());
+
+dat = new SlashCommandBuilder()
+.setName('image')
+.setDescription('image Generation');
+commands.push(dat.toJSON());
+
 
 (async () => {
 	try {
@@ -39,17 +46,29 @@ commands.push(dat.toJSON());
 	}
 })();
 
+let tex ;
+async function getImageUlr(){
+const sdk =  require("./imageGenerator");
+ tex = await sdk("A old nepal");
+console.log(tex);
+}
+
+
 
 client.on(Events.InteractionCreate, async (interaction) => {
     console.log(interaction.isChatInputCommand());
     if (!interaction.isChatInputCommand()) return;
-   
+
     if(interaction.commandName == 'ping'){
         await interaction.reply(`pong | ${interaction.user.username}`);
     }
     if(interaction.commandName == "hii"){
         await interaction.reply(`Hii ! ${interaction.user.username} k xa vai`);
     }
+	if(interaction.commandName == "image"){
+		getImageUlr();
+		await interaction.reply(`${tex}`)
+	}
 })
 
 
@@ -63,6 +82,10 @@ client.login(token);
 app.listen(4000 , () => {
     console.log(`Server started at port 3000`);
 })
+
+
+// router.post('https://api.edenai.run/v2/image/generation', sdkGen());
+
 
 app.get("/", (req , res) => {
     res.send("hello world")
